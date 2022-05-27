e74dcdc86f72f6d4ae047ba09638e0dc56cb57a5
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-05-27 09:55:57 +0530

    created chat basic app

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,45 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Chat from './Chat'
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+  const [username,setUsername]=useState("")
+  const [room,setRoom]=useState("")
+  const [showChat,setShowChat]=useState(false)
+
+  const joinRoom=()=>{
+    if(username!=="" && room!==""){
+      socket.emit("join_room",room);
+      setShowChat(true)
+    }
+  }
 
   return (
     <div className="App">
-      <h1>This is my chat app</h1>
+      {!showChat?(
+        <div className='joinChatContainer'>
+          <h3>Join a Chat</h3>
+          <input
+          type="text"
+          placeholder='John...'
+          onChange={(event)=>{
+            setUsername(event.target.value)
+          }}
+          ></input>
+          <input type="text"
+          placeholder='Room Id..'
+          onChange={(event)=>{
+            setRoom(event.target.value);
+          }}
+          ></input>
+          <button onClick={joinRoom}>Join a Room</button>
+          </div>
+      ):(
+        <Chat socket={socket} username={username} room={room}/>
+      )}
     </div>
   )
 }
